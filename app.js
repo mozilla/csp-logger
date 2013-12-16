723871a8427d7bac82874193449971f44c4e389b
Gvn Lazar Suntop <gavin@gsuntop.com>
2013-12-16 15:21:52 -0800

    externalize config, domain whitelist, source blacklist

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 var http = require('http');
 var sequelize = require('sequelize');
+var fs = require('fs');
 
-var sequelizeDB = new sequelize('csp', 'root', 'root', {
-  dialect: 'mysql', // or 'sqlite', 'postgres', 'mariadb'
-  port: 8889 // or 5432 (for postgres)
+var config = JSON.parse(fs.readFileSync('./env.json', {
+  encoding: 'utf8'
+}));
+
+var sequelizeDB = new sequelize(config.dbName, config.dbUsername, config.dbPassword, {
+  host: config.dbHost,
+  dialect: config.dbDialect, // or 'sqlite', 'postgres', 'mariadb'
+  port: config.dbPort
 });
 
 // Authenticate and connect to DB
@@ -33,7 +39,7 @@ sequelizeDB.sync({
   //force: true // Remove existing tables and recreate them
 }).complete(function (err) {
   if ( !! err) {
-    console.log('An error occurred while creating the table.');
+    console.error('An error occurred while creating the table.');
   } else {
     console.log('Table created.');
   }
@@ -72,8 +78,26 @@ http.createServer(function (req, res) {
       body = Buffer.concat(bodyParts, bytes).toString('utf8');
       json = JSON.parse(body);
       console.log('Attempting to store violation:');
-      console.log(json);
-      storeViolation(json['csp-report']);
+
+      var violatorDomain = json['csp-report']['document-uri'].match(/\/\/(.*)\//)[1];
+      var allowedDomain = false;
+      var allowedSource = true;
+
+      config.domainWhitelist.forEach(function (domain) {
+        if (violatorDomain === domain) {
+          allowedDomain = true;
+        }
+      });
+
+      config.sourceBlacklist.forEach(function (source) {
+        if (json['csp-report']['source-file'] === source) {
+          allowedSource = false;
+        }
+      });
+
+      if (allowedDomain && allowedSource) {
+        storeViolation(json['csp-report']);
+      }
     } catch (ex) {
       console.log(body);
     }
@@ -90,10 +114,6 @@ http.createServer(function (req, res) {
   res.writeHead(200, {
     'content-type': 'text/plain'
   });
-
-  if (req.url === '/csp') {
-    console.log('csp request made');
-  }
 
   res.end();
 }).listen(2600);
